63d57ac602b0d3a2f467329ab0ae59f13f1c64d5
MARTI <MARTI@MARTINX>
2023-08-11 15:25:15 -0600

    VERSION 1.96

---
+++ src/main/java/com/mycompany/prueba2_v2/MAINX.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba2_v2;
+
+/**
+ *
+ * @author MARTI
+ */
+public class MAINX {
+    public static void main(String[] args) {
+        
+        Menu RRR=new Menu();
+        
+        RRR.setVisible(true);
+        RRR.setLocationRelativeTo(null);
+        
+        
+        
+    }
+}
